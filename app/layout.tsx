9d1050e3761d732018e563f49524eccecd3b7304
zerzkhane <zezkhanemourad@gmail.com>
2023-11-29 00:01:03 +0100

    Add NavBar

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
+
+
+export const metadata: Metadata = {
+  title: 'Travel',
+  description: 'Travel App for Campaning',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className='relative overflow-hidden'>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
