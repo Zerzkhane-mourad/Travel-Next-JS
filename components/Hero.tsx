94784b97a6208d3d5e0e2d622b92febd62758f0a
zerzkhane <zerzkhanemourad@gmail.com>
2023-12-01 23:32:44 +0100

    add contenent in hero and custumize

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,11 @@ import Button from './Button'
 
 const Hero = () => {
     return (
-        <section className='max-container padding-container flex flex-col py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row gap-20 border-2  border-red-500 ' >
+        <section className='max-container padding-container flex flex-col py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row gap-20 ' >
 
             <div className='hero-map' />
             <div className='relative z-20 flex flex-1 flex-col xl:w-1/2 '>
-                <Image src="/capm.svg" alt='camp' width={50} height={50} className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]' />
+                <Image src="/camp.svg" alt='camp' width={50} height={50} className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]' />
                 <h1 className='bold-52 lg:bold-88'>Putuk Truno Camp Area</h1>
                 <p className='reguler-16 mt-6 text-gray-30 xl:max-w-[520px]'>
                     We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
@@ -35,11 +35,34 @@ const Hero = () => {
                 </div>
 
                 <div className='flex w-full gap-3 sm:flex-row'>
-                    <Button type='button' title='Dowload App' variant='btn_dark_green' />
+                    <Button type='button' title='Dowload App' variant='btn_green' />
                     <Button type='button' title='How we Work ?' icon='/play.svg' variant='btn_white_text' />
-
                 </div>
+            </div>
 
+            <div className='relative flex flex-1 items-start'>
+                <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8 ' >
+
+                    <div className='flex flex-col '>
+                        <div className='flexBetween'>
+                            <p className='regular-16 text-gray-20'>Location</p>
+                            <Image src="/close.svg" alt='close' width={24} height={24} />
+                        </div>
+                        <p className='bold-20 text-white'>Aguas Calientes</p>
+                    </div>
+
+                    <div className='flexBetween'>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 block text-gray-20'>Distance</p>
+                            <p className='bold-20 text-white'>167 mi</p>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 block text-gray-20'>Elevation</p>
+                            <p className='bold-20 text-white'>120 km</p>
+                        </div>
+                    </div>
+                
+                </div>
             </div>
 
         </section>
